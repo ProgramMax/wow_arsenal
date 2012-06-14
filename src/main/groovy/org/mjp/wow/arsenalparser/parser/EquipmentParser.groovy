1309c9a0de1f6dba385aa04f962b92310b4a739a
Michael Preuhs <michael.preuhs@gmail.com>
2012-06-14 12:10:56 +0200

    Add import for missing classes

--- src/main/groovy/org/mjp/wow/arsenalparser/parser/EquipmentParser.groovy
+++ src/main/groovy/org/mjp/wow/arsenalparser/parser/EquipmentParser.groovy
@@ -1,5 +1,12 @@
 package org.mjp.wow.arsenalparser.parser
 
+import net.htmlparser.jericho.Element
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+import org.mjp.wow.arsenalparser.domain.CharEquipment
+
 class EquipmentParser {
     final static LOGGER = LoggerFactory.getLogger(EquipmentParser.class)
 
