ba260187fbffbbbc602afd78fb150657b14ece02
mpreuhs <michael.preuhs@gmail.com>
2014-03-07 11:09:35 +0100

    Revision of the class.

--- src/main/groovy/org/mjp/wow/arsenalparser/io/web/Downloader.groovy
+++ src/main/groovy/org/mjp/wow/arsenalparser/io/web/Downloader.groovy
@@ -1,57 +1,93 @@
 package org.mjp.wow.arsenalparser.io.web
 
+import java.nio.file.attribute.BasicFileAttributes
+import java.nio.file.Files
+import java.nio.file.FileSystem
+import java.nio.file.FileSystems
+import java.nio.file.Path
+import java.nio.file.Paths
+
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 class Downloader {
-    final def LOGGER = LoggerFactory.getLogger(this.class)
+    final static def LOGGER = LoggerFactory.getLogger(Downloader.class)
     
-    def sourceUrl
+    def targetFolder
     
-    Downloader(def sourceUrl) {
-        this.sourceUrl = sourceUrl
+    /**
+     * Class constructor.
+     * @param targetFolder is the local folder name, where to download the file. This URL should ends with "/".
+     */
+    Downloader(def targetFolder) {
+        this.targetFolder = targetFolder
+    }
+    
+    /**
+     * Download method gets a file from sourceUrl and saves it to the folder targetUrl.
+     * @param sourceUrl is the file URL to download.
+     */
+    def download(final downloadUrl) {
+        LOGGER.debug('download address:      {}', downloadUrl)
+
+        Path targetFolderPath = Paths.get(this.targetFolder)
+        new File(targetFolderPath.toUri()).mkdirs()
+        Path targetFilePath = Paths.get(targetFolderPath.toString() + File.separatorChar + this.getFileName(downloadUrl))
+
+        LOGGER.debug('temporary target file: {}', targetFilePath)
+
+        def filename = new FileOutputStream(new File(targetFilePath.toUri()))
+        def outputFile = new BufferedOutputStream(filename)
+
+        LOGGER.debug('download start')
+        outputFile << new URL(this.fixLocalUrl(downloadUrl)).openStream()
+        LOGGER.debug('download complete')
+
+        outputFile.close();
     }
 
-    /**
-     * Download method.
-     * destAddress is the destination address in string format
-     */
-    def download(final def localDir) {
-        def targetFileDir = new File(localDir)
-        if (!targetFileDir.exists()) {
-                targetFileDir.mkdirs()
-        }
-		
-        def filename = localDir + '/' + sourceUrl.tokenize("/")[-1]
-        def file = new FileOutputStream(filename)
-        def out = new BufferedOutputStream(file)
-        
-        LOGGER.debug('download address: {}', sourceUrl)
-        LOGGER.debug('temp file:        {}', filename)
-        
-        LOGGER.info('start download')
-        out << new URL(sourceUrl).openStream()
-        LOGGER.info('download ready')
-        
-        out.close()
-    }
-    
-    def String checkUrl() {
-        def prefixProtocolList = this.sourceUrl.split(':')
+    private String fixLocalUrl(final sourceUrl) {
+        def prefixProtocolList = sourceUrl.split(':')
         
         def protocol
+        def fixedSourceUrl
         if (prefixProtocolList.size() == 2) {
             protocol = prefixProtocolList[0]
+            fixedSourceUrl = sourceUrl
         } else {
-            this.sourceUrl = 'file:' + this.sourceUrl
-            
             protocol = 'file'
+            fixedSourceUrl = 'file:' + sourceUrl
         }
-            
-        LOGGER.debug('used url protocol: {}', protocol)
-        LOGGER.info('source url:        {}', this.sourceUrl)
         
-        return protocol
+        LOGGER.info('source url:        {}', sourceUrl)
+        LOGGER.debug('fixed source url:  {}', fixedSourceUrl)
+        LOGGER.debug('url protocol:      {}', protocol)
+        
+        return fixedSourceUrl
+    }
+    
+    private String getFileName(final url) {
+        if (url.endsWith('/')) {
+            'index.html'
+        } else {
+            url.tokenize('/')[-1]
+        }
+    }
+    
+    static void main(def args) {
+        def dl = new Downloader('temp/')
+        // http://commons.apache.org/proper/commons-math/userguide/analysis.html
+        dl.download('data/http/commons.apache.org/proper/commons-math/userguide/analysis.html')
+
+        Path path = Paths.get('temp/analysis.html')
+        BasicFileAttributes attrs = Files.readAttributes(path, BasicFileAttributes.class)
+        LOGGER.debug('file attribute, isRegularFile():  {}', attrs.isRegularFile())    // true
+        LOGGER.debug('file attribute, isDirectory():    {}', attrs.isDirectory())      // false
+        LOGGER.debug('file attribute, isSymbolicLink(): {}', attrs.isSymbolicLink())   // false
+        LOGGER.debug('file attribute, isOther():        {}', attrs.isOther())          // false
+        LOGGER.debug('file attribute, size():           {}', attrs.size())             // 59666
+
+        new File('temp/analysis.html').delete()
     }
 }
 
